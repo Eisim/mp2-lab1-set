6cb91c285b92f0259263ce141e5a5ee4b723f54c
Eisim <koctik.w@mail.ru>
2022-09-19 21:15:53 +0300

    stoped on operator 'or'

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,8 +1,20 @@
 #include <gtest.h>
-
+//my changes
+#include"tbitfield.h"
 //eto ochen' vazhnii komentarii
 
 int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    TBitField a(32),b(17),c(0);
+    std::cin >> a;
+    std::cout << "a)" << a<<std::endl;
+    std::cin >> b;
+    std::cout<< "\n" << "b)" << b << std::endl;
+
+
+    c = a | b;
+    std::cout <<"c)" << c;
+    return 0;
+
+    //::testing::InitGoogleTest(&argc, argv);
+    //return RUN_ALL_TESTS();
 }
